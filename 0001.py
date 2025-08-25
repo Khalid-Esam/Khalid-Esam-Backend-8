f1f408c7f2716557c2c926f1065a12a8d66fbe86
Khalid-Esam <khalidesam151@gmail.com>
2025-08-25 18:09:33 +0300

    Add files via upload

---
+++ 0001.py
@@ -0,0 +1,9 @@
+first_name="Khalid"
+second_name="Esam"
+GPA=0.33
+date_of_birth=2005
+age=19
+print("Name:",first_name,second_name)
+print("GPA:",GPA)
+print("date of birth:",date_of_birth)
+print("age:",age)
